dd22a519a15590c20f8ca824e7546b3353800060
Juliet Heye <julietheye@pa-dhcp-10-120-245-174.gp-vpn.northwestern.private>
2020-04-03 18:55:27 -0500

    calculate and plot 95% confidence interval instead of std dev, expanded time window

--- actPasAnalysis.m
+++ actPasAnalysis.m
@@ -15,6 +15,8 @@
 
 %% normalize data
     td = trial_data;
+    %rebin TD to 50ms bins
+    %td = binTD(trial_data, 10);
     %only look at sorted units
     td.S1_spikes(:,td.S1_unit_guide(:,2)==0) = [];
     td.S1_unit_guide(td.S1_unit_guide(:,2)==0,:) = [];
@@ -26,13 +28,13 @@
     params.width = 0.01;
     td = smoothSignals(td,params);
     %larger smoothing window bec low firing neurons 
+    td.S1_spikes = normalize(td.S1_spikes, 'range');
     params1.signals = {'S1_spikes','all'};
-    params1.width = 0.1;
+    params1.width = 0.01;
     params1.calc_rate = true;
     td = smoothSignals(td,params1);
     
-    
-    clear params
+    clear params params1
 
 %% split into trials
 
@@ -72,8 +74,8 @@
      
 %% trim active and passive
 
-    td_act = trimTD(td_act, {'idx_movement_on',-100}, {'idx_movement_on',100});
-    td_bump = trimTD(td_bump, {'idx_movement_on',-100}, {'idx_movement_on',100});
+    td_act = trimTD(td_act, {'idx_movement_on',-0.5/td.bin_size}, {'idx_movement_on',0.5/td.bin_size});
+    td_bump = trimTD(td_bump, {'idx_movement_on',-0.5/td.bin_size}, {'idx_movement_on',0.5/td.bin_size});
     
 %% find average muscle signals
 
@@ -84,22 +86,56 @@
     paramsPas.conditions = 'bumpDir';
     paramsPas.add_std = true;
     avgDataPass = trialAverage(td_bump,paramsPas);
+    
+    %calculate 95% confidence interval from std
+    for i=1:numel(avgDataAct)
+        %active
+        ActEmgStd = cell2mat({avgDataAct(i).emg_std});
+        avgDataAct(i).emg_confInt = 1.96 .* ActEmgStd ./ sqrt(numel(ActEmgStd(:,1)));
+        
+        ActMuscleVelStd = cell2mat({avgDataAct(i).muscle_vel_std});
+        avgDataAct(i).muscle_vel_confInt = 1.96 .* ActMuscleVelStd ./ sqrt(numel(ActMuscleVelStd(:,1)));
+        
+        ActMuscleLenStd = cell2mat({avgDataAct(i).muscle_len_std});
+        avgDataAct(i).muscle_len_confInt = 1.96 .* ActMuscleLenStd ./ sqrt(numel(ActMuscleLenStd(:,1)));
+        
+        ActSpikesStd = cell2mat({avgDataAct(i).S1_spikes_std});
+        avgDataAct(i).S1_spikes_confInt = 1.96 .* ActSpikesStd ./ sqrt(numel(ActSpikesStd(:,1)));
+        
+        %passive
+        PasEmgStd = cell2mat({avgDataPass(i).emg_std});
+        avgDataPass(i).emg_confInt = 1.96 .* PasEmgStd ./ sqrt(numel(PasEmgStd(:,1)));
+        
+        PasMuscleVelStd = cell2mat({avgDataPass(i).muscle_vel_std});
+        avgDataPass(i).muscle_vel_confInt = 1.96 .* PasMuscleVelStd ./ sqrt(numel(PasMuscleVelStd(:,1)));
+        
+        PasMuscleLenStd = cell2mat({avgDataPass(i).muscle_len_std});
+        avgDataPass(i).muscle_len_confInt = 1.96 .* PasMuscleLenStd ./ sqrt(numel(PasMuscleLenStd(:,1)));
+        
+        PasSpikesStd = cell2mat({avgDataPass(i).S1_spikes_std});
+        avgDataPass(i).S1_spikes_confInt = 1.96 .* PasSpikesStd ./ sqrt(numel(PasSpikesStd(:,1)));
+    end
 
-clear paramsAct paramsPas td_act td_bump
+    clear paramsAct paramsPas td_act td_bump avgData
+    clear i ActEmgStd ActMuscleVelStd ActMuscleLenStd ActSpikesStd
+    clear PasEmgStd PasMuscleVelStd PasMuscleLenStd PasSpikesStd
 
 %% plot avg emg signals
 
-    %create time/muscle/direction arrays
-    timeArray = [-100:5:100];
-    muscleArrayEMG = [5,6,7,12,22,3,17];
-    muscleArrayM = [8,9,10,38,3,19,14];
+    %define time window
+    timeArray = [-500:(td.bin_size*1000):500]; %ms
+    tStart = 1; %bins
+    tEnd = numel(avgDataAct(1).emg(:,1)); %bins
+    %define muscles
+    muscleArrayEMG = [5,6,7,12,22,3,17]; %from EMG list
+    muscleArrayM = [8,9,10,38,3,19,14]; %from motion tracking list
     muscleNames = string({'deltAnt','deltMid','deltPost','triMid','biLat','FCU','ECU'});
-    dir = {avgDataAct(1).tgtDir,avgDataAct(2).tgtDir,avgDataAct(3).tgtDir,avgDataAct(4).tgtDir,avgDataAct(5).tgtDir,avgDataAct(6).tgtDir,avgDataAct(7).tgtDir,avgDataAct(8).tgtDir};
+    %define directions
+    dir = {avgDataAct.tgtDir};
     directions = string(unique(cell2mat(dir)));
     
     %plot 'em
     figEMG = figure('Name','EMG');
-    title('EMG')
     figMuscleVel = figure('Name','Muscle Velocity');
     figMuscleLen = figure('Name','Muscle Length');
     figUnits = figure('Name','Neural Units');
@@ -114,8 +150,9 @@ clear paramsAct paramsPas td_act td_bump
             subplot(numel(muscleArrayEMG),numel(directions),k);
             title(strcat(muscleNames(i),directions(j)))
             hold on
-            errorbar(timeArray, avgDataAct(j).emg(81:121,muscleArrayEMG(i)), avgDataAct(j).emg_std(81:121,muscleArrayEMG(i)))
-            errorbar(timeArray, avgDataPass(j).emg(81:121,muscleArrayEMG(i)), avgDataPass(j).emg_std(81:121,muscleArrayEMG(i)))
+            errorbar(timeArray, avgDataAct(j).emg(tStart:tEnd,muscleArrayEMG(i)), avgDataAct(j).emg_confInt(tStart:tEnd,muscleArrayEMG(i)))
+            errorbar(timeArray, avgDataPass(j).emg(tStart:tEnd,muscleArrayEMG(i)), avgDataPass(j).emg_confInt(tStart:tEnd,muscleArrayEMG(i)))
+            axis([-500 500 0 0.25]);
             sgtitle('EMG');
             if k==1
                 legend('Active','Passive','Position',[0 0.8 0.12 0.1])
@@ -126,8 +163,9 @@ clear paramsAct paramsPas td_act td_bump
             subplot(numel(muscleArrayM),numel(directions),k);
             title(strcat(muscleNames(i),directions(j)))
             hold on
-            errorbar(timeArray, avgDataAct(j).muscle_vel(81:121,muscleArrayM(i)), avgDataAct(j).muscle_vel_std(81:121,muscleArrayM(i)))
-            errorbar(timeArray, avgDataPass(j).muscle_vel(81:121,muscleArrayM(i)), avgDataPass(j).muscle_vel_std(81:121,muscleArrayM(i)))
+            errorbar(timeArray, avgDataAct(j).muscle_vel(tStart:tEnd,muscleArrayM(i)), avgDataAct(j).muscle_vel_confInt(tStart:tEnd,muscleArrayM(i)))
+            errorbar(timeArray, avgDataPass(j).muscle_vel(tStart:tEnd,muscleArrayM(i)), avgDataPass(j).muscle_vel_confInt(tStart:tEnd,muscleArrayM(i)))
+            axis([-500 500 0 0.8]);
             sgtitle('Muscle Velocity');
             if k==1
                 legend('Active','Passive','Position',[0 0.8 0.12 0.1])
@@ -138,8 +176,9 @@ clear paramsAct paramsPas td_act td_bump
             subplot(numel(muscleArrayM),numel(directions),k);
             title(strcat(muscleNames(i),directions(j)))
             hold on
-            errorbar(timeArray, avgDataAct(j).muscle_len(81:121,muscleArrayM(i)), avgDataAct(j).muscle_len_std(81:121,muscleArrayM(i)))
-            errorbar(timeArray, avgDataPass(j).muscle_len(81:121,muscleArrayM(i)), avgDataPass(j).muscle_len_std(81:121,muscleArrayM(i)))
+            errorbar(timeArray, avgDataAct(j).muscle_len(tStart:tEnd,muscleArrayM(i)), avgDataAct(j).muscle_len_confInt(tStart:tEnd,muscleArrayM(i)))
+            errorbar(timeArray, avgDataPass(j).muscle_len(tStart:tEnd,muscleArrayM(i)), avgDataPass(j).muscle_len_confInt(tStart:tEnd,muscleArrayM(i)))
+            axis([-500 500 0 0.8]);
             sgtitle('Muscle Length');
             if k==1
                 legend('Active','Passive','Position',[0 0.8 0.12 0.1])
@@ -148,17 +187,18 @@ clear paramsAct paramsPas td_act td_bump
     end
     
     k=0;
-    for i=1:7
+    for i=11:17
         for j=1:numel(directions)
             k=k+1;
             
-            %emg plots
+            %neural unit plots
             set(0,'CurrentFigure',figUnits)
             subplot(7,numel(directions),k);
             title(strcat('Neuron',int2str(i),' Direction',directions(j)))
             hold on
-            errorbar(timeArray, avgDataAct(j).S1_spikes(81:121,i), avgDataAct(j).S1_spikes_std(81:121,i))
-            errorbar(timeArray, avgDataPass(j).S1_spikes(81:121,i), avgDataPass(j).S1_spikes_std(81:121,i))
+            errorbar(timeArray, avgDataAct(j).S1_spikes(tStart:tEnd,i), avgDataAct(j).S1_spikes_confInt(tStart:tEnd,i))
+            errorbar(timeArray, avgDataPass(j).S1_spikes(tStart:tEnd,i), avgDataPass(j).S1_spikes_confInt(tStart:tEnd,i))
+            axis([-500 500 0 10]);
             sgtitle('Neural Units');
             if k==1
                 legend('Active','Passive','Position',[0 0.8 0.12 0.1])
@@ -167,7 +207,7 @@ clear paramsAct paramsPas td_act td_bump
     end
     
     
-    clear i j k directions muscleArrayEMG muscleArrayM muscleNames timeArray
+    clear i j k tStart tEnd dir directions muscleArrayEMG muscleArrayM muscleNames timeArray
     
 
     
